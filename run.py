a9d6ffbb44f225c570cf49cc6e79fbb6f074dbc6
Andy Pierce <spudos16@gmail.com>
2023-12-21 13:44:35 +0000

    add version number

--- run.py
+++ run.py
@@ -89,7 +89,8 @@ def print_for_the_win():
     print_centered('11 players.  The formation will be determined based on what players you pick.')
     print_centered('You will then be able to see the match stats and finally, the outcome of the')
     print_centered('game.')
-    print_centered('Good luck, I hope you enjoy "For the Win!"' + Style.RESET_ALL)
+    print_centered('Good luck, I hope you enjoy "For the Win!"')
+    print_centered('V1.69' + Style.RESET_ALL)
     print()
     press_any_key_to_continue()
 
